15334c52b0ef43d204cb1495de1fdfaa15805083
Kenny Nguyen <knguyen111601@gmail.com>
2021-11-18 11:07:28 -0800

    got the profile things all connected and displated. Some styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,27 @@
-const Profile = (props) =>{
+import { useState, useContext, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { GlobalCtx } from "../App"
+
+const Profile = (props) => {
+
+    const {gState, setGState} = useContext(GlobalCtx)
+    const {username, pfp, bio, id} = gState
+
+    const loaded = () =>{
+       return <div className="profileSection">
+           <img src={pfp} alt="profile picture"/>
+           <h1>{username}</h1>
+           <p>{bio}</p>
+       </div>
+    }
+
+    const notLoaded = () =>{
+        return <h1>No one yet</h1>
+    }
+
     return <div className="profile">
         <h1>Profile</h1>
+        {username ? loaded() : notLoaded()}
     </div>
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import { GlobalCtx } from "../App"
 import {useState, useContext} from "react"
 import {useNavigate} from "react-router-dom"
+import Profile from "../components/Profile"
 const Login = (props) =>{
     const { gState, setGState, token } = useContext(GlobalCtx);
     const { url } = gState;
@@ -32,7 +33,7 @@ const Login = (props) =>{
         console.log(data)
         // local storage to keep token so it doesn't disappear when refreshing
         window.localStorage.setItem("token", JSON.stringify(data))
-        setGState({...gState, token: data.token})
+        setGState({...gState, token: data.token, username: data.username, pfp: data.pfp, bio: data.bio, id: data.id})
         setForm({
             username: "",
             password: ""
